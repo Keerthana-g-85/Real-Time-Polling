7ed08f2e2edd788aa107522612619842f5e0f7de
Keerthana G <keerthana@CELSYSBLR000337.local>
2026-08-11 12:51:03 +0530

    intilize server

---
+++ server/server.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config();
+const app = express()
+app.use(cors({origin : "http://localhost:5173/"}))
+
+app.listen(process.env.PORT , ()=>{
+    console.log("Server Started")
+})
