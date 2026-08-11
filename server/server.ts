cfd0dd5880a53c34aac4ae8a516a23eb4ba0410b
Keerthana G <keerthana@CELSYSBLR000337.local>
2026-08-11 17:39:19 +0530

    intilize backend graphql setup

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,20 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import { connection } from "./database.js";
+import { buildSchema } from "type-graphql";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@as-integrations/express4";
 
 dotenv.config();
-const app = express()
-app.use(cors({origin : "http://localhost:5173/"}))
-await connection()
-app.listen(process.env.PORT , ()=>{
-    console.log("Server Started")
-})
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173/" }));
+const schema = await buildSchema({ resolvers: [] });
+const server = new ApolloServer({ schema });
+server.start();
+await connection();
+app.use("graphql", express.json(), expressMiddleware(server));
+app.listen(process.env.PORT, () => {
+  console.log("Server Started");
+});
