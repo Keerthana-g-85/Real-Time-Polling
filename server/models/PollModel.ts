0339c610b046638b3bdfa1ff0c1f97df5015b5d8
Keerthana G <keerthana@CELSYSBLR000337.local>
2026-08-11 14:49:12 +0530

    poll model creation

---
+++ server/models/PollModel.ts
@@ -0,0 +1,48 @@
+import { Field, GraphQLISODateTime, ID, ObjectType } from "type-graphql";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import Users from "./UsersModel.js";
+
+@ObjectType()
+@Entity()
+export default class Poll {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Field(() => String)
+  @Column({ type: "varchar" })
+  poll_name!: string;
+
+  @Field(() => String)
+  @Column({ type: "varchar" })
+  question!: string;
+
+  @Field(() => Users)
+  @ManyToOne(() => Users)
+  @JoinColumn({ name: "user_id" })
+  user_id!: Users;
+
+  @Field(() => GraphQLISODateTime)
+  @Column({ type: "timestamptz" })
+  expire_time!: Date;
+
+  @Field(() => String)
+  @Column({ type: "varchar" })
+  status!: string;
+
+  @Field(() => Date)
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @Field(() => Date)
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
