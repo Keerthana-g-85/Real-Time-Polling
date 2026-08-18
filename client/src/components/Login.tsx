902c4a5b177e452a0ccb9fd30177a9646e2d4ea7
Keerthana G <keerthana@CELSYSBLR000337.local>
2026-08-18 12:58:54 +0530

    Login and register page ui change

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,12 +1,23 @@
-import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import useApi from "../Api";
 import { LOGIN_USER } from "../graphql/Mutation/LOGIN_USER";
 import { useMutation } from "@tanstack/react-query";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { ME } from "../graphql/Query/Me";
 import { useDispatch } from "react-redux";
 import { setUser } from "../redux/LoginSlice";
+import login_img from "../assets/login_img.png";
+import MailIcon from "@mui/icons-material/Mail";
+import LockIcon from "@mui/icons-material/Lock";
 
 export default function Login() {
   const [login, setLogin] = useState({ email: "", password: "" });
@@ -54,7 +65,7 @@ export default function Login() {
     });
     console.log(response);
     const meResponse = await useApi({ query: ME });
-    console.log(meResponse)
+    console.log(meResponse);
     return meResponse.me;
   }
 
@@ -68,56 +79,156 @@ export default function Login() {
 
   return (
     <>
-      <Box
-        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-      >
-        <Paper
-          elevation={4}
-          sx={{ height: "250px", width: "400px", p: 4, mt: "250px" }}
+      <Grid container spacing={2} sx={{bgcolor:"#e3e7f3"}}>
+        <Grid
+          size={{
+            xs: 12,
+            md: 6,
+          }}
         >
-          <Typography>Email :</Typography>
-          <TextField
-            fullWidth
-            id={err.email ? "outlined-error" : "outlined-basic"}
-            variant="outlined"
-            type="text"
-            value={login.email}
-            error={err.email}
-            helperText={message.email}
-            onChange={(e) => {
-              setLogin({ ...login, email: e.target.value });
-              setErr({ ...err, email: false });
-              setMessage({ ...message, email: "" });
+          <Box
+            sx={{
+              backgroundImage: `url(${login_img})`,
+              backgroundSize: "100% 100%",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              width: "100%",
+              height: "100vh",
             }}
-          />
-          <Typography>Password :</Typography>
-          <TextField
-            fullWidth
-            id={err.password ? "outlined-error" : "outlined-basic"}
-            variant="outlined"
-            type="password"
-            value={login.password}
-            error={err.password}
-            helperText={message.password}
-            onChange={(e) => {
-              setLogin({ ...login, password: e.target.value });
-              setErr({ ...err, password: false });
-              setMessage({ ...message, password: "" });
-            }}
-          />
+          ></Box>
+        </Grid>
+        <Grid>
           <Box
             sx={{
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
+              width: "100%",
+              height: "100vh",
             }}
           >
-            <Button onClick={handleLogin} sx={{ mt: 3 }} variant="contained">
-              Login
-            </Button>
+            <Paper
+              elevation={1}
+              sx={{
+                p: 6,
+                borderRadius: 3,
+                width: 500,
+                height: 370,
+                ml: 20,
+              }}
+            >
+              <Stack spacing={4}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    mb: 3,
+                    fontWeight: 700,
+                  }}
+                >
+                  <Typography
+                    variant="h4"
+                    sx={{
+                      color: "#517cf1",
+                      fontFamily: '"Segoe UI"',
+                      fontWeight: "600",
+                    }}
+                  >
+                    Welcome Back to Live VoteFlow
+                  </Typography>
+                  <Typography
+                    sx={{
+                      fontWeight: 1,
+                      fontFamily: '"Segoe UI"',
+                      color: "#354675",
+                      fontSize: 13,
+                    }}
+                  >
+                    Vote, share your opinion, and discover what others think
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{ display: "flex", alignItems: "flex-end", gap: 2, p: 0 }}
+                >
+                  <MailIcon />
+                  <TextField
+                    fullWidth
+                    placeholder="Email "
+                    id={err.email ? "outlined-error" : "outlined-basic"}
+                    variant="outlined"
+                    type="text"
+                    value={login.email}
+                    error={err.email}
+                    helperText={message.email}
+                    onChange={(e) => {
+                      setLogin({ ...login, email: e.target.value });
+                      setErr({ ...err, email: false });
+                      setMessage({ ...message, email: "" });
+                    }}
+                  />
+                </Box>
+                <Box
+                  sx={{ display: "flex", alignItems: "flex-end", gap: 2, p: 0 }}
+                >
+                  <LockIcon />
+                  <TextField
+                    fullWidth
+                    placeholder="Password"
+                    id={err.password ? "outlined-error" : "outlined-basic"}
+                    variant="outlined"
+                    type="password"
+                    value={login.password}
+                    error={err.password}
+                    helperText={message.password}
+                    onChange={(e) => {
+                      setLogin({ ...login, password: e.target.value });
+                      setErr({ ...err, password: false });
+                      setMessage({ ...message, password: "" });
+                    }}
+                  />
+                </Box>
+              </Stack>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Button
+                  onClick={handleLogin}
+                  sx={{
+                    mt: 3,
+                    bgcolor: "#6d92f7",
+                    width: 200,
+                    borderRadius: 3,
+                    mb: 3,
+                  }}
+                  variant="contained"
+                >
+                  Login
+                </Button>
+                <Link to="/register">
+                  <Typography
+                    sx={{
+                      fontWeight: 1,
+                      fontFamily: '"Segoe UI"',
+                      color: "#354675",
+                      fontSize: 13,
+                    }}
+                  >
+                    Don't have an account yet ? Register
+                  </Typography>
+                </Link>
+              </Box>
+            </Paper>
           </Box>
-        </Paper>
-      </Box>
+        </Grid>
+      </Grid>
     </>
   );
 }
--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,9 +1,21 @@
-import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import { USER_REGISTER } from "../graphql/Mutation/USER_REGISTER";
 import { useMutation } from "@tanstack/react-query";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import useApi from "../Api";
+import login_img from "../assets/login_img.png";
+import PersonIcon from "@mui/icons-material/Person";
+import MailIcon from "@mui/icons-material/Mail";
+import LockIcon from "@mui/icons-material/Lock";
 
 export default function Register() {
   const [register, setRegister] = useState({
@@ -118,73 +130,175 @@ export default function Register() {
 
   return (
     <>
-      <Box
-        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-      >
-        <Paper
-          elevation={4}
-          sx={{ height: "300px", width: "400px", p: 4, mt: "250px" }}
+      <Grid container spacing={2} sx={{ bgcolor: "#e3e7f3" }}>
+        <Grid
+          size={{
+            xs: 12,
+            md: 6,
+          }}
         >
-          <Typography>Name :</Typography>
-          <TextField
-            fullWidth
-            id={err.name ? "outlined-error" : "outlined-basic"}
-            variant="outlined"
-            type="text"
-            value={register.name}
-            error={err.name}
-            helperText={message.name}
-            onChange={(e) => {
-              setRegister({ ...register, name: e.target.value });
-              setErr({ ...err, name: false });
-              setMessage({ ...message, name: "" });
+          <Box
+            sx={{
+              backgroundImage: `url(${login_img})`,
+              backgroundSize: "100% 100%",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              width: "100%",
+              height: "100vh",
             }}
-          />
-
-          <Typography>Email :</Typography>
-          <TextField
-            fullWidth
-            id={err.email ? "outlined-error" : "outlined-basic"}
-            variant="outlined"
-            type="text"
-            value={register.email}
-            error={err.email}
-            helperText={message.email}
-            onChange={(e) => {
-              setRegister({ ...register, email: e.target.value });
-              setErr({ ...err, email: false });
-              setMessage({ ...message, email: "" });
-            }}
-          />
-
-          <Typography>Password :</Typography>
-          <TextField
-            fullWidth
-            id={err.password ? "outlined-error" : "outlined-basic"}
-            variant="outlined"
-            type="password"
-            value={register.password}
-            error={err.password}
-            helperText={message.password}
-            onChange={(e) => {
-              setRegister({ ...register, password: e.target.value });
-              setErr({ ...err, password: false });
-              setMessage({ ...message, password: "" });
-            }}
-          />
+          ></Box>
+        </Grid>
+        <Grid>
           <Box
             sx={{
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
+              width: "100%",
+              height: "100vh",
             }}
           >
-            <Button onClick={handleRegister} sx={{ mt: 3 }} variant="contained">
-              Register
-            </Button>
+            <Paper
+              elevation={1}
+              sx={{
+                p: 6,
+                borderRadius: 3,
+                width: 500,
+                height: 430,
+                ml: 20,
+              }}
+            >
+              <Stack spacing={4}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    mb: 3,
+                    fontWeight: 700,
+                  }}
+                >
+                  <Typography
+                    variant="h4"
+                    sx={{
+                      color: "#517cf1",
+                      fontFamily: '"Segoe UI"',
+                      fontWeight: "600",
+                    }}
+                  >
+                    Welcome to Live VoteFlow
+                  </Typography>
+                  <Typography
+                    sx={{
+                      fontWeight: 1,
+                      fontFamily: '"Segoe UI"',
+                      color: "#354675",
+                      fontSize: 13,
+                    }}
+                  >
+                    Sign in to share your opinion and participate in live polls.
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{ display: "flex", alignItems: "flex-end", gap: 2, p: 0 }}
+                >
+                  <PersonIcon />
+                  <TextField
+                    placeholder="Name"
+                    fullWidth
+                    id={err.name ? "outlined-error" : "outlined-basic"}
+                    variant="outlined"
+                    type="text"
+                    value={register.name}
+                    error={err.name}
+                    helperText={message.name}
+                    onChange={(e) => {
+                      setRegister({ ...register, name: e.target.value });
+                      setErr({ ...err, name: false });
+                      setMessage({ ...message, name: "" });
+                    }}
+                  />
+                </Box>
+                <Box
+                  sx={{ display: "flex", alignItems: "flex-end", gap: 2, p: 0 }}
+                >
+                  <MailIcon />
+                  <TextField
+                    placeholder="Email"
+                    fullWidth
+                    id={err.email ? "outlined-error" : "outlined-basic"}
+                    variant="outlined"
+                    type="text"
+                    value={register.email}
+                    error={err.email}
+                    helperText={message.email}
+                    onChange={(e) => {
+                      setRegister({ ...register, email: e.target.value });
+                      setErr({ ...err, email: false });
+                      setMessage({ ...message, email: "" });
+                    }}
+                  />
+                </Box>
+                <Box
+                  sx={{ display: "flex", alignItems: "flex-end", gap: 2, p: 0 }}
+                >
+                  <LockIcon />
+                  <TextField
+                  placeholder="Password"
+                    fullWidth
+                    id={err.password ? "outlined-error" : "outlined-basic"}
+                    variant="outlined"
+                    type="password"
+                    value={register.password}
+                    error={err.password}
+                    helperText={message.password}
+                    onChange={(e) => {
+                      setRegister({ ...register, password: e.target.value });
+                      setErr({ ...err, password: false });
+                      setMessage({ ...message, password: "" });
+                    }}
+                  />
+                </Box>
+              </Stack>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Button
+                  onClick={handleRegister}
+                  sx={{
+                    mt: 3,
+                    bgcolor: "#6d92f7",
+                    width: 200,
+                    borderRadius: 3,
+                    mb: 3,
+                  }}
+                  variant="contained"
+                >
+                  Register
+                </Button>
+                <Link to="/">
+                  <Typography
+                    sx={{
+                      fontWeight: 1,
+                      fontFamily: '"Segoe UI"',
+                      color: "#354675",
+                      fontSize: 13,
+                    }}
+                  >
+                    Account already exist? Login
+                  </Typography>
+                </Link>
+              </Box>
+            </Paper>
           </Box>
-        </Paper>
-      </Box>
+        </Grid>
+      </Grid>
     </>
   );
 }
